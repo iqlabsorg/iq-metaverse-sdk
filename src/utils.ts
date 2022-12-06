382c828b7c08c17028bc2e329fc36d0aacc83e4d
Norberts Jēgers <norberts.jegers@blockvis.com>
2022-12-06 17:04:26 +0200

    build fix

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,6 @@
 import { assetClasses } from './constants';
 
-export const pick = <T, K extends keyof T>(obj: T, names: readonly K[]): Pick<T, K> => {
+export const pick = <T extends object, K extends keyof T>(obj: T, names: readonly K[]): Pick<T, K> => {
   const result = {} as Pick<T, K>;
   let idx = 0;
   while (idx < names.length) {
