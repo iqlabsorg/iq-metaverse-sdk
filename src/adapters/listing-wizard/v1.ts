e86978b3c209c798049eb0c4fdf496c5a0f2efb4
Norberts Jēgers <norberts.jegers@blockvis.com>
2023-02-22 21:39:39 +0200

    listing wizard supports delegated listing creation

--- src/adapters/listing-wizard/v1.ts
+++ src/adapters/listing-wizard/v1.ts
@@ -1,10 +1,11 @@
 import { AccountId } from 'caip';
-import { BigNumberish, ContractTransaction } from 'ethers';
+import { BigNumber, BigNumberish, BytesLike, ContractTransaction } from 'ethers';
 import { Adapter } from '../../adapter';
 import { AddressTranslator } from '../../address-translator';
 import { ContractResolver } from '../../contract-resolver';
 import { IListingTermsRegistry, ListingWizardV1 } from '../../contracts';
 import { Listings } from '../../contracts/contracts/listing/listing-manager/ListingManager';
+import { Assets } from '../../contracts/contracts/metahub/core/IMetahub';
 import { AssetListingParams } from '../../types';
 
 export class ListingWizardAdapterV1 extends Adapter {
@@ -26,13 +27,8 @@ export class ListingWizardAdapterV1 extends Adapter {
     assetListingParams: AssetListingParams,
     listingTerms: IListingTermsRegistry.ListingTermsStruct,
   ): Promise<ContractTransaction> {
-    const { assets, params, maxLockPeriod, immediatePayout } = assetListingParams;
-
-    const encodedAssets = assets.map(x => this.encodeAsset(x));
-    const listingParams: Listings.ParamsStruct = {
-      lister: this.accountIdToAddress(params.lister),
-      configurator: this.accountIdToAddress(params.configurator),
-    };
+    const { encodedAssets, listingParams, maxLockPeriod, immediatePayout } =
+      this.prepareListingParams(assetListingParams);
 
     return this.contract.createListingWithTerms(
       encodedAssets,
@@ -42,5 +38,63 @@ export class ListingWizardAdapterV1 extends Adapter {
       immediatePayout,
       universeId,
     );
+  }
+
+  /**
+   * Creates new asset listing (delegated).
+   * @param universeId Universe ID.
+   * @param assetListingParams Listing params.
+   * @param listingTerms Listing terms.
+   * @param delegatedListingSignature Delegated Listing ECDSA signature ABI encoded (v,r,s)(uint8, bytes32, bytes32).
+   */
+  async delegatedCreateListingWithTerms(
+    universeId: BigNumberish,
+    assetListingParams: AssetListingParams,
+    listingTerms: IListingTermsRegistry.ListingTermsStruct,
+    delegatedListingSignature: BytesLike,
+  ): Promise<ContractTransaction> {
+    const { encodedAssets, listingParams, maxLockPeriod, immediatePayout } =
+      this.prepareListingParams(assetListingParams);
+
+    return this.contract.delegatedCreateListingWithTerms(
+      encodedAssets,
+      listingParams,
+      listingTerms,
+      maxLockPeriod,
+      immediatePayout,
+      universeId,
+      delegatedListingSignature,
+    );
+  }
+
+  /**
+   * Get the current nonce of lister for delegated listing.
+   * @param lister Lister account ID.
+   */
+  async getDelegatedListingCurrentNonce(lister: AccountId): Promise<BigNumber> {
+    return this.contract.getDelegatedListingCurrentNonce(this.accountIdToAddress(lister));
+  }
+
+  /**
+   * Returns the domain separator used in the encoding of the signature for permit, as defined by EIP712.
+   */
+  async DOMAIN_SEPARATOR(): Promise<string> {
+    return this.contract.DOMAIN_SEPARATOR();
+  }
+
+  private prepareListingParams(assetListingParams: AssetListingParams): {
+    encodedAssets: Assets.AssetStruct[];
+    listingParams: Listings.ParamsStruct;
+    maxLockPeriod: BigNumberish;
+    immediatePayout: boolean;
+  } {
+    const { assets, params, maxLockPeriod, immediatePayout } = assetListingParams;
+    const encodedAssets = assets.map(x => this.encodeAsset(x));
+    const listingParams: Listings.ParamsStruct = {
+      lister: this.accountIdToAddress(params.lister),
+      configurator: this.accountIdToAddress(params.configurator),
+    };
+
+    return { encodedAssets, listingParams, maxLockPeriod, immediatePayout };
   }
 }
